11123c08ee1fa61875477da65a2e4b5f048aafe9
Lucas Merat <lucas.merat@gmail.com>
2019-04-17 10:18:16 -0500

    Sorted projects by updated date in list of projects

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import ProjectSummary from './ProjectSummary'
-import { Link } from 'react-router-dom'
+import React from "react";
+import ProjectSummary from "./ProjectSummary";
+import { Link } from "react-router-dom";
 
-const ProjectList = ({projects}) =>{
-    console.log(projects)
-    return(
-        <div className="project-list section">
-            {projects && projects.map(project =>{
-                return(
-                    <Link to={"/project/" + project.id} key={project.id}><ProjectSummary project={project}/></Link>
-                )
-            })}
-        </div>
-    )
-}
+const ProjectList = ({ projects }) => {
+    let sortedProjects;
+  if (projects.length > 0) {
+    sortedProjects = [...projects].sort((a, b) => {
+      return b.updatedAt.seconds > a.updatedAt.seconds ? 1 : -1;
+    });
+    console.log(sortedProjects)
+  }
+  return (
+    <div className="project-list section">
+      {sortedProjects &&
+        sortedProjects.map(project => {
+          return (
+            <Link to={"/project/" + project.id} key={project.id}>
+              <ProjectSummary project={project} />
+            </Link>
+          );
+        })}
+    </div>
+  );
+};
 
-export default ProjectList
+export default ProjectList;
